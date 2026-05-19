f169422452d651daa49eb708691add261f56d3ec
Olena Mustafina <toysamiy.mail@gmail.com>
2026-05-19 16:49:31 +0300

    feat: Add About section with base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./sections/Header/Header";
+import About from "./sections/About/About";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <About />
+      </main>
     </>
   );
 };
---
+++ src/sections/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return (
+    <section class="about-section">
+      <div className="container">
+        <h1 className="about-section__title"> Olena </h1>
+        <p className="about-section__content">
+          Frontend developer with a focus on building engaging & accessible
+          digital products
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
