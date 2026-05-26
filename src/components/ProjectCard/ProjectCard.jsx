ee490e56806b9e4f842d3001e81b7fdcafb0037c
Olena Mustafina <toysamiy.mail@gmail.com>
2026-05-26 10:32:29 +0300

    feat: Implement ProjectCard includes styles, add color variables for themes and integrate test card to Projects component

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,55 @@
+import "./ProjectCard.scss";
+
+const ProjectCard = ({ data }) => {
+  const { title, description, image, stack, demo, link } = data;
+
+  return (
+    <article className="project-card">
+      <div className="project-card__image">
+        <img src={image} alt={`Screenshot of ${title} project`} />
+      </div>
+
+      <div className="project-card__content">
+        <h2 className="project-card__title">{title}</h2>
+        <p className="project-card__description">{description}</p>
+
+        {stack && (
+          <ul className="project-card__stack" aria-label="Technologies used">
+            {stack.map((tech, index) => (
+              <li key={index} className="project-card__stack-item">
+                {tech}
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div className="project-card__links">
+          {demo && (
+            <a
+              href={demo}
+              target="_blank"
+              rel="noreferrer"
+              className="project-card__btn-link"
+              aria-label={`View live demo of ${title}`}
+            >
+              Demo
+            </a>
+          )}
+          {link && (
+            <a
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+              className="project-card__btn-link"
+              aria-label={`View source code of ${title} on GitHub`}
+            >
+              GitHub
+            </a>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Projects.scss";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+
+const testData = {
+  title: "Garry Potter",
+  description:
+    "Lorem150 ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
+  image: "./images/garry.jpg",
+  stack: ["HTML", "SCSS"],
+  demo: "#",
+  link: "#",
+};
 
 const Projects = () => {
   return (
@@ -7,6 +18,7 @@ const Projects = () => {
       <div className="container">
         <h1> Projects </h1>
         <p>Card</p>
+        <ProjectCard data={testData} />
       </div>
     </section>
   );
