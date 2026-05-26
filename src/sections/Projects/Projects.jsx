a794dbba9e5f55105b0d93ea54402e68654e995d
Olena Mustafina <toysamiy.mail@gmail.com>
2026-05-26 17:26:52 +0300

    feat: Update Projects component includes styles

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,24 +1,32 @@
-import React from "react";
-import "./Projects.scss";
+import { useState, useEffect } from "react";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
-
-const testData = {
-  title: "Garry Potter",
-  description:
-    "Lorem150 ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
-  image: "./images/garry.jpg",
-  stack: ["HTML", "SCSS"],
-  demo: "#",
-  link: "#",
-};
+import "./Projects.scss";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/projects.json")
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
-    <section id="projects" className="projects-section">
+    <section
+      id="projects"
+      className="projects-section section"
+      aria-labelledby="projects-title"
+    >
       <div className="container">
-        <h1> Projects </h1>
-        <p>Card</p>
-        <ProjectCard data={testData} />
+        <h2 className="section__title" id="projects-title">
+          Projects
+        </h2>
+        <div className="projects-section__grid" role="list">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} data={project} />
+          ))}
+        </div>
       </div>
     </section>
   );
