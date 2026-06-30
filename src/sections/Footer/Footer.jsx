8bcf3898a31b4b8c9d7ead477ebbe97c3143defd
Olena Mustafina <toysamiy.mail@gmail.com>
2026-06-30 15:11:01 +0300

    feat: Update footer includes styles and typography

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,40 +1,46 @@
 import React from "react";
+import Icon from "@/components/Icons/Icons";
 import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer">
-        <div className="footer__container">
-          <span className="footer__copy">
-            © {new Date().getFullYear()} | Built with React & SCSS
-          </span>
-
-          <div className="footer__links">
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__info">
+          <div className="footer__cta">
+            <p>Come with me now:</p>
+            <a href="mailto:olena.codes@proton.me" className="footer__link">
+              olena.codes@proton.me
+            </a>
+          </div>
+          <div className="footer__social">
             <a
-              href="https://github.com/"
+              href="https://github.com/LenaM777"
               target="_blank"
               rel="noreferrer"
-              className="footer__link"
+              className="footer__link btn-touch-target"
+              aria-label="GitHub profile (opens in a new tab)"
             >
-              GitHub
+              <Icon name="github" />
             </a>
             <a
               href="https://linkedin.com/"
               target="_blank"
               rel="noreferrer"
-              className="footer__link"
+              className="footer__link btn-touch-target"
+              aria-label="LinkedIn (opens in a new tab)"
             >
-              LinkedIn
-            </a>
-            <a href="mailto:your.email@example.com" className="footer__link">
-              Email
+              <Icon name="linkedin" />
             </a>
           </div>
         </div>
-      </footer>
-      ;
-    </div>
+        <span className="footer__copy">
+          &copy; {new Date().getFullYear()} | Designed & Engineered by Olena M.
+          Powered by React. Inspired by Mono Sans typeface. Focused on Web
+          Accessibility. All rights reserved.
+        </span>
+      </div>
+    </footer>
   );
 };
 
