f5eaea1dff1125186d7a0af909fb6ba98ed3f173
Olena Mustafina <toysamiy.mail@gmail.com>
2026-07-27 12:24:20 +0300

    feat: Add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./styles/global.scss";
 import App from "./App.jsx";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
